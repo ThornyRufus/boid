d1c263fe3bf9b6837c2fa12235d704520894c85e
RufusThorny <seba181099@gmail.com>
2020-04-27 00:29:43 +0200

    first rule : median direction

--- main.py
+++ main.py
@@ -1,41 +1,85 @@
-import pygame, sys
+import pygame, sys, math, random, statistics
 pygame.init()
 
 back_color = pygame.Color('#86BBD8')
 boid_color = pygame.Color('#2F4858')
-object_list = []
 
 
 class boid:
-    def __init__(self,surface,size,posx, posy):
+    def __init__(self,surface,size,posx, posy,speed=1):
         self.surface = surface
         self.size = size
+        '''
         self.posx = posx
         self.posy = posy
+        '''
+        self.posx = random.randrange(0,1920)
+        self.posy = random.randrange(0,1080)
+        self.speed = speed
+        self.total_angle = random.randrange(0,360)
+        self.objective = 0
+        self.alpha = math.radians(self.total_angle % 360)
+
+    def find_objective(self,object_list):
+
+        for object in object_list:
+            near_list = []
+            vect = []
+            sum = 0
+            vect.append(self.posx - object.posx)
+            vect.append(self.posy - object.posy)
+            dist = math.sqrt( math.pow(vect[0],2) + math.pow(vect[1],2) )
+
+            if dist < 300:
+                near_list.append(object)
+            if len(near_list) == 0:
+                self.objective = 0
+            else:
+                angle_list = []
+                for object in near_list:
+                    angle_list.append(object.alpha)
+                self.objective = statistics.median(angle_list)
+
+    def actualize(self):
+        self.alpha = math.radians(self.total_angle % 360)
+        self.beta = math.radians((self.total_angle + 90) % 360)
+        self.delta = math.radians((self.total_angle + 270) % 360)
 
     def draw(self):
-        body_points = [(self.posx - (self.size / 2), self.posy), (self.posx + (self.size / 2), self.posy),
-                       (self.posx, self.posy + self.size)]
-        body = pygame.draw.polygon(self.surface,boid_color,body_points,0)
+        self.actualize()
+        self.body_points = [(self.posx + ( math.cos(self.alpha) * self.size ), self.posy + ( math.sin(self.alpha) * self.size ) ),
+                                    (self.posx + ( math.cos(self.beta) * (self.size/3) ), self.posy + ( math.sin(self.beta) * (self.size/3) ) ),
+                                    (self.posx + ( math.cos(self.delta) * (self.size/3) ), self.posy + ( math.sin(self.delta) * (self.size/3) ) )]
+        body = pygame.draw.polygon(self.surface,boid_color,self.body_points,0)
 
+    def move(self,speed=10): #calculer les nouveaux body points selon l'angle
 
-class arena:
+        if self.objective > self.alpha:
+            self.total_angle += 1
+        if self.objective < self.alpha:
+            self.total_angle -= 1
+        self.actualize()
+        self.posx +=  ( math.cos(self.alpha) * speed )
+        self.posy += ( math.sin(self.alpha) * speed )
+
+class arena: #classe principale, initialisation et game loop
     def __init__(self,x,y):
         self.x = x
         self.y = y
+        self.object_list = []
         #pygame.display.set_icon()
         pygame.display.set_caption('BOID simulation')
         screen = pygame.display.set_mode([self.x,self.y])
         screen.fill(back_color)
 
-        for i in range(20):
-            object_list.append(boid(screen,20,50+30*i,50))
+        for i in range(120):
+            self.object_list.append(boid(screen,25,50+i*15,50+i*3))
 
         pygame.display.update()
         clock = pygame.time.Clock()
 
         while True:
-            clock.tick(50)
+            clock.tick(60)
 
             # Process events
             for event in pygame.event.get():
@@ -45,11 +89,13 @@ class arena:
             #clear screen
             screen.fill(back_color)
 
-            # Check input
+            for object in self.object_list:
+                object.find_objective(self.object_list)
+                #object.total_angle += random.randrange(-5,5)
+                object.move()
 
-            for i in range(len(object_list)):
-                object_list[i].posy += 2*(i+1)
-            for object in object_list:
+
+            for object in self.object_list: #a changer, methode pour remetre les objets dans le cadre
                 if object.posx > self.x:
                     object.posx = 0
                 if object.posx < 0:
@@ -60,7 +106,7 @@ class arena:
                     object.posy = 0
 
             # Move objects ...
-            for object in object_list:
+            for object in self.object_list: #draw, pas de probleme
                 object.draw()
 
             # Draw objects ...
@@ -71,7 +117,4 @@ class arena:
         pygame.event.clear()
         pygame.event.wait()
 '''
-
-
-
-display = arena(800,400)
+display = arena(1920,1080)
